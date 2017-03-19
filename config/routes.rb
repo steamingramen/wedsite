ced790c7b6d8d10523575f780abc7675a476d6a5
steamingramen <jrichmond124@gmail.com>
2017-03-19 12:40:10 -0400

    Initial checkin of Hotels page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'hotels/index'
+
+  get 'hotel/index'
+
   root 'home#index'
 
   resources :rsvp, only: [:index, :new, :create], as: 'rsvps'
   resources :registries, only: :index
   resources :photos, only: :index
   resources :faq, only: :index, as: 'faqs'
+  resources :hotels, only: :index
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
